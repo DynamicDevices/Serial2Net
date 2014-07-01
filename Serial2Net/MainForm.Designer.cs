a3be49cc77740d864381d5b1b9da799dc0e30571
Alex Lennon <ajlennon@dynamicdevices.co.uk>
2014-07-01 10:41:06 +0100

    Initial Commit

---
+++ Serial2Net/MainForm.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace Serial2Net
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.comboBoxSerialPort = new System.Windows.Forms.ComboBox();
+            this.labelSerialPort = new System.Windows.Forms.Label();
+            this.labelTargetIP = new System.Windows.Forms.Label();
+            this.textBoxIPAddress = new System.Windows.Forms.TextBox();
+            this.labelTargetPort = new System.Windows.Forms.Label();
+            this.textBoxTargetPort = new System.Windows.Forms.TextBox();
+            this.buttonStartStop = new System.Windows.Forms.Button();
+            this.comboBoxBaudRate = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxLog = new System.Windows.Forms.TextBox();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // comboBoxSerialPort
+            // 
+            this.comboBoxSerialPort.FormattingEnabled = true;
+            this.comboBoxSerialPort.Location = new System.Drawing.Point(174, 19);
+            this.comboBoxSerialPort.Name = "comboBoxSerialPort";
+            this.comboBoxSerialPort.Size = new System.Drawing.Size(100, 21);
+            this.comboBoxSerialPort.TabIndex = 0;
+            // 
+            // labelSerialPort
+            // 
+            this.labelSerialPort.AutoSize = true;
+            this.labelSerialPort.Location = new System.Drawing.Point(58, 22);
+            this.labelSerialPort.Name = "labelSerialPort";
+            this.labelSerialPort.Size = new System.Drawing.Size(55, 13);
+            this.labelSerialPort.TabIndex = 1;
+            this.labelSerialPort.Text = "Serial Port";
+            // 
+            // labelTargetIP
+            // 
+            this.labelTargetIP.AutoSize = true;
+            this.labelTargetIP.Location = new System.Drawing.Point(301, 22);
+            this.labelTargetIP.Name = "labelTargetIP";
+            this.labelTargetIP.Size = new System.Drawing.Size(91, 13);
+            this.labelTargetIP.TabIndex = 2;
+            this.labelTargetIP.Text = "Target IP address";
+            // 
+            // textBoxIPAddress
+            // 
+            this.textBoxIPAddress.Location = new System.Drawing.Point(417, 19);
+            this.textBoxIPAddress.Name = "textBoxIPAddress";
+            this.textBoxIPAddress.Size = new System.Drawing.Size(100, 20);
+            this.textBoxIPAddress.TabIndex = 3;
+            this.textBoxIPAddress.Text = "127.0.0.1";
+            // 
+            // labelTargetPort
+            // 
+            this.labelTargetPort.AutoSize = true;
+            this.labelTargetPort.Location = new System.Drawing.Point(301, 49);
+            this.labelTargetPort.Name = "labelTargetPort";
+            this.labelTargetPort.Size = new System.Drawing.Size(60, 13);
+            this.labelTargetPort.TabIndex = 4;
+            this.labelTargetPort.Text = "Target Port";
+            // 
+            // textBoxTargetPort
+            // 
+            this.textBoxTargetPort.Location = new System.Drawing.Point(417, 49);
+            this.textBoxTargetPort.Name = "textBoxTargetPort";
+            this.textBoxTargetPort.Size = new System.Drawing.Size(100, 20);
+            this.textBoxTargetPort.TabIndex = 5;
+            this.textBoxTargetPort.Text = "10446";
+            // 
+            // buttonStartStop
+            // 
+            this.buttonStartStop.Location = new System.Drawing.Point(576, 22);
+            this.buttonStartStop.Name = "buttonStartStop";
+            this.buttonStartStop.Size = new System.Drawing.Size(75, 23);
+            this.buttonStartStop.TabIndex = 6;
+            this.buttonStartStop.Text = "Start";
+            this.buttonStartStop.UseVisualStyleBackColor = true;
+            this.buttonStartStop.Click += new System.EventHandler(this.buttonStartStop_Click);
+            // 
+            // comboBoxBaudRate
+            // 
+            this.comboBoxBaudRate.FormattingEnabled = true;
+            this.comboBoxBaudRate.Items.AddRange(new object[] {
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "19200",
+            "38400",
+            "57600",
+            "115200"});
+            this.comboBoxBaudRate.Location = new System.Drawing.Point(174, 46);
+            this.comboBoxBaudRate.Name = "comboBoxBaudRate";
+            this.comboBoxBaudRate.Size = new System.Drawing.Size(100, 21);
+            this.comboBoxBaudRate.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Baud Rate";
+            // 
+            // textBoxLog
+            // 
+            this.textBoxLog.Location = new System.Drawing.Point(12, 89);
+            this.textBoxLog.Multiline = true;
+            this.textBoxLog.Name = "textBoxLog";
+            this.textBoxLog.Size = new System.Drawing.Size(715, 268);
+            this.textBoxLog.TabIndex = 9;
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.Location = new System.Drawing.Point(619, 367);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(108, 13);
+            this.linkLabel1.TabIndex = 10;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "Dynamic Devices Ltd";
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(739, 389);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.textBoxLog);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBoxBaudRate);
+            this.Controls.Add(this.buttonStartStop);
+            this.Controls.Add(this.textBoxTargetPort);
+            this.Controls.Add(this.labelTargetPort);
+            this.Controls.Add(this.textBoxIPAddress);
+            this.Controls.Add(this.labelTargetIP);
+            this.Controls.Add(this.labelSerialPort);
+            this.Controls.Add(this.comboBoxSerialPort);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "MainForm";
+            this.Text = "Serial2Net";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBoxSerialPort;
+        private System.Windows.Forms.Label labelSerialPort;
+        private System.Windows.Forms.Label labelTargetIP;
+        private System.Windows.Forms.TextBox textBoxIPAddress;
+        private System.Windows.Forms.Label labelTargetPort;
+        private System.Windows.Forms.TextBox textBoxTargetPort;
+        private System.Windows.Forms.Button buttonStartStop;
+        private System.Windows.Forms.ComboBox comboBoxBaudRate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxLog;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ Serial2Net/MainForm.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO.Ports;
+using System.Text;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Sockets;
+using PortListener.Core.Utilities;
+
+namespace Serial2Net
+{
+    public partial class MainForm : Form
+    {
+        private delegate void LogHandler(string text);
+
+        private TcpClient _connection;
+        private SerialPort _port;
+        private NetworkStream _stream;
+        private byte[] _tcpdata = new byte[1024];
+
+        public MainForm()
+        {
+            InitializeComponent();
+
+            string[] arrPorts = SerialPort.GetPortNames();
+            comboBoxSerialPort.Items.Clear();
+            foreach (var port in arrPorts)
+                comboBoxSerialPort.Items.Add(port);
+            if (arrPorts.Length > 0)
+                comboBoxSerialPort.SelectedIndex = 0;
+
+            comboBoxBaudRate.SelectedIndex = 4;
+        }
+
+        private void buttonStartStop_Click(object sender, EventArgs e)
+        {
+            if(buttonStartStop.Text == "Start")
+            {
+                try
+                {
+                    _port = new SerialPort((string)comboBoxSerialPort.SelectedItem, int.Parse((string)comboBoxBaudRate.SelectedItem), Parity.None, 8, StopBits.One);
+                    _port.DataReceived += new SerialDataReceivedEventHandler(_port_DataReceived);
+                    _port.ReceivedBytesThreshold = 8;                    
+                    _port.Open();
+                } catch(Exception ex)
+                {
+                    MessageBox.Show("Couldn't open port " + (string) comboBoxSerialPort.SelectedItem);
+                    return;
+                }
+
+                try
+                {
+                    _connection = new TcpClient(textBoxIPAddress.Text, int.Parse(textBoxTargetPort.Text));
+                    _stream = _connection.GetStream();
+                    byte[] tcpdata = new byte[1024];
+                    _stream.BeginRead(tcpdata, 0, tcpdata.Length, new AsyncCallback(TcpReader), null);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Couldn't connect to " + textBoxTargetPort.Text + ":" + textBoxIPAddress.Text);
+                    return;
+                }
+                buttonStartStop.Text = "Stop";
+            }
+            else
+            {
+                if(_port.IsOpen)
+                    _port.Close();
+
+                if(_connection.Connected)
+                    _connection.Close();
+
+                buttonStartStop.Text = "Start";               
+            }              
+        }
+
+        void _port_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            int rxlen = _port.BytesToRead;
+            byte[] data = new byte[rxlen];
+            _port.Read(data, 0, rxlen);
+
+            Log("S->N: " + StringHelper.ToHexString(data, 0, rxlen));
+
+            _stream.Write(data, 0, rxlen);
+        }
+
+        void TcpReader(IAsyncResult ar)
+        {
+            int rxbytes = _stream.EndRead(ar);
+
+            if (rxbytes > 0)
+            {
+                _port.Write(_tcpdata, 0, rxbytes);
+                Log("N->S: " + StringHelper.ToHexString(_tcpdata, 0, rxbytes));
+            }
+
+            if (rxbytes >= 0)
+            {
+                _stream.BeginRead(_tcpdata, 0, _tcpdata.Length, new AsyncCallback(TcpReader), null);
+            }
+        }
+
+        void Log(string text)
+        {
+            if(InvokeRequired)
+            {
+                this.Invoke(new LogHandler(Log), new object[] {text});
+                return;
+            }
+            textBoxLog.Text += text + "\r\n";
+            textBoxLog.SelectionStart = textBoxLog.Text.Length - 1;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("http://www.dynamicdevices.co.uk");
+        }
+    }
+}
